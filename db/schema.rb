0bf8ee3f62682f0b8792d5364b82c99feb5c54dc
hanna_nyman <hannanyman.sk@gmail.com>
2019-02-12 10:50:59 +0100

    Generates menu table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_094217) do
+ActiveRecord::Schema.define(version: 2019_02_12_094929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
